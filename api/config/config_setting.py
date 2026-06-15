1bb467f92f7c29b2636c1753aa235cbf0260f872
gowtham943 <gowthamkandasamy32@gmail.com>
2026-06-15 14:15:05 +0530

    Added AI dependecies.

--- api/config/config_setting.py
+++ api/config/config_setting.py
@@ -7,6 +7,11 @@ class config_setting(BaseSettings):
     JWT_SECRET_KEY: str
     JWT_ALGORITHM: str = "HS256"
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 60
+    OLLAMA_HOST: str = "http://localhost:11434"
+    GEMINI_API_KEY: str
+    GEMINI_MODEL: str = "gemini-2.5-flash"
+    EMBEDDING_MODEL: str = "all-minilm:v2"
+    MCP_SERVER_SSE_URL: str = "http://habit_api:8000/mcp/sse"
 
     model_config = SettingsConfigDict(env_file=".env", extra="ignore")
 
